15b45e15bc3f04108b20acf059e692249426394e
f.c.j.bergman@student.tue.nl <salamandertje2002>
2023-05-02 17:24:26 +0200

    Fixed a mistake and got the right dataset with burglary in barnet

--- Main.py
+++ Main.py
@@ -22,30 +22,33 @@ def make_db():
                                 """)
 
 def load_data(directory):
-    deletec_value = []
+
     for folder in os.listdir(directory):
+        print(folder)
         for file in os.listdir('E:\\data\\data\\'+folder):
-            print(file)
-            if 'city-of-london' in file:
-                data = pd.read_csv('E:\\data\\data\\'+folder+'\\'+file)
-                for line in data.iterrows():
-                    values = ', '.join("'" + str(x) + "'" for x in line[1])
-                    data_line = values.split(',')
-                    if len(data_line) == 10:
-                        data_line.append(data_line[9])
-                        data_line[9] = ' NaN'
-                        data_line.append(' NaN')
-                        values = ', '.join("'" + str(x) + "'" for x in data_line)
-                    try:
-                        c.execute('INSERT INTO accidents (Crime_ID, Month, Reported_by, Falls_within, Longitude, Latitude, Location, LSOA_code, LSOA_name, Crime_type, Last_outcome_category, Context) VALUES ('+values+ ')')
-                    except:
-                        deletec_value.append(values)
-    print(deletec_value)
+            deletec_value = []
+            data = pd.read_csv('E:\\data\\data\\'+folder+'\\'+file)
+            for line in data.iterrows():
+                values = ', '.join("'" + str(x) + "'" for x in line[1])
+                data_line = values.split(',')
+                if len(data_line) == 10:
+                    data_line.append(data_line[9])
+                    data_line[9] = ' NaN'
+                    data_line.append(' NaN')
+                    values = ', '.join("'" + str(x) + "'" for x in data_line)
+                try:
+                    c.execute('INSERT INTO accidents (Crime_ID, Month, Reported_by, Falls_within, Longitude, Latitude, Location, LSOA_code, LSOA_name, Crime_type, Last_outcome_category, Context) VALUES ('+values+ ')')
+                except:
+                    deletec_value.append(values)
+            #/print(deletec_value)
 
 def clean_data():
     c.execute("""DELETE FROM accidents
-                WHERE Crime_type IS NOT 'Burglary'
+                WHERE LSOA_name NOT LIKE '%Barnet%'
                 """)
+    c.execute("""DELETE FROM accidents
+                WHERE Crime_type IS NOT 'Burglary'""")
+
 
 #make_db()
 #load_data('E:\\data\\data')
