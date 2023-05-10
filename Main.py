889dabfa755d37f9f7fa91e92cd90e1ed02b4edf
f.c.j.bergman@student.tue.nl <salamandertje2002>
2023-05-10 11:10:01 +0200

    Fixed the last problems with the DB and cleaned data

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ def load_data(directory):
             deletec_value = []
             data = pd.read_csv('E:\\data\\data\\'+folder+'\\'+file)
             for line in data.iterrows():
-                values = ', '.join("'" + str(x) + "'" for x in line[1])
+                values = ', '.join("'" + str(x).replace("'", "-") + "'" for x in line[1])
                 data_line = values.split(',')
                 if len(data_line) == 10:
                     data_line.append(data_line[9])
@@ -40,7 +40,7 @@ def load_data(directory):
                     c.execute('INSERT INTO accidents (Crime_ID, Month, Reported_by, Falls_within, Longitude, Latitude, Location, LSOA_code, LSOA_name, Crime_type, Last_outcome_category, Context) VALUES ('+values+ ')')
                 except:
                     deletec_value.append(values)
-            #/print(deletec_value)
+    print(deletec_value)
 
 def clean_data():
     c.execute("""DELETE FROM accidents
@@ -48,10 +48,14 @@ def clean_data():
                 """)
     c.execute("""DELETE FROM accidents
                 WHERE Crime_type IS NOT 'Burglary'""")
+    c.execute("""ALTER TABLE accidents
+                    DROP COLUMN Reported_by""")
+    c.execute("""ALTER TABLE accidents
+                    DROP COLUMN context""")
 
 
-#make_db()
-#load_data('E:\\data\\data')
+# make_db()
+# load_data('E:\\data\\data')
 clean_data()
 
 
